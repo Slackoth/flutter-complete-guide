78f3bb99d4a665dd31f01536b17ea379efd0c8f7
Slackoth <00082318@uca.edu.sv>
2023-02-05 22:02:21 -0600

    [ADDED] Stateful behavior

--- projects/basics/lib/main.dart
+++ projects/basics/lib/main.dart
@@ -1,24 +1,39 @@
 import 'package:basics/log/getLogger.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 void main() {
   runApp(const BasicsApp());
 }
 
-class BasicsApp extends StatelessWidget  {
-  static final log = getLogger();
-
+class BasicsApp extends StatefulWidget {
   const BasicsApp({super.key});
 
-  void answerQuestion() { log.i('answeringTheQuestion'); }
+  @override
+  State<StatefulWidget> createState() {
+    return BasicsAppState();
+  }
+}
+
+class BasicsAppState extends State<BasicsApp> {
+  static final Logger log = getLogger();
+
+  int questionIndex = 0;
+
+  void answerQuestion() { 
+    setState(() { questionIndex++; });
+    log.i('indexQuestion: $questionIndex'); 
+  }
 
   @override
   Widget build(BuildContext context) {
+    List<String> questions = ['What\'s Mr. Peanutbutter birthday?', 'Who\'s that dog?'];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Flutter Basics')),
         body: Column(children: <Widget>[
-          Text('Question...'),
+          Text(questions[questionIndex]),
           ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
           ElevatedButton(onPressed: answerQuestion, child: Text('Answer 2')),
           ElevatedButton(onPressed: answerQuestion, child: Text('Answer 3')),
