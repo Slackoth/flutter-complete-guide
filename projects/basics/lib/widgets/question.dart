156ea01d5c15721b2f444ce2e4b1e3a6e06e8e7d
Slackoth <00082318@uca.edu.sv>
2023-02-07 22:59:58 -0600

    [ADDED] Container widget surrounding the Text widget

--- projects/basics/lib/widgets/question.dart
+++ projects/basics/lib/widgets/question.dart
@@ -8,6 +8,13 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity, // All space available
+      margin: EdgeInsets.all(10), // Margin in all directions (top, right, bottom, left)
+      child: Text(
+        questionText, 
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,)
+      );
   }
 }
