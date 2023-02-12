fa8175f036b657ec513399a7880ff63c282e2995
Slackoth <00082318@uca.edu.sv>
2023-02-12 10:17:37 -0600

    [ADDED] Amount not a number and not equal to 0 validation

--- projects/expenses_app/lib/widgets/transaction_form.dart
+++ projects/expenses_app/lib/widgets/transaction_form.dart
@@ -1,11 +1,31 @@
+import 'package:expenses_app/utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 class TransactionForm extends StatelessWidget { 
+  static final Logger log = Utils.getLogger();
+  
   final Function addNewTransaction;
   final TextEditingController titleController = TextEditingController();
   final TextEditingController amountController = TextEditingController();
   
   TransactionForm({super.key, required this.addNewTransaction});
+
+  void _onSubmittedTextField(String value) {
+    final String title = titleController.text;
+    final double? amount = double.tryParse(amountController.text);
+
+    if(amount == null || title.isEmpty) {
+      log.i('Amount not a number o title empty');
+      return;
+    }
+    else if(amount == 0) {
+      log.i('Amount equal to 0');
+      return;
+    }
+
+    addNewTransaction(title, amount);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,15 +38,16 @@ class TransactionForm extends StatelessWidget {
             TextField(
               decoration: const InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: _onSubmittedTextField,
             ),
             TextField(
               decoration: const InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: const TextInputType.numberWithOptions(decimal: true, signed: false),
+              onSubmitted: _onSubmittedTextField,
             ),
             TextButton(
-              onPressed: () {
-                addNewTransaction(titleController.text, double.parse(amountController.text));
-              },
+              onPressed: () { _onSubmittedTextField('submitButton'); },
               style: TextButton.styleFrom(foregroundColor: Colors.purple),
               child: const Text('Add Transaction')
             )
