8175f453719987bcbe18609e79f10e9d832601a7
Slackoth <00082318@uca.edu.sv>
2023-02-21 20:06:28 -0600

    [ADDED] Bottom tab bar navgation

--- projects/meals_app/lib/screens/tab_screen.dart
+++ projects/meals_app/lib/screens/tab_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/favorites_screen.dart';
 
@@ -12,22 +10,56 @@ class TabScreen extends StatefulWidget {
 }
 
 class _TabScreenState extends State<TabScreen> {
+  int _selectedScreen = 0;
+  final List<Map<String, dynamic>> _screens = [
+    { 'screen': const CategoriesScreen(), 'title': 'Catagories' },
+    { 'screen': const FavoritesScreen(), 'title': 'Favorites' }
+  ];
+
+  void _selectScreen(int index) {
+    setState(() { _selectedScreen = index; });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(initialIndex: 0, length: 2, child: 
-      Scaffold(
-        appBar: AppBar(
-          title: const Text('DeliMeals'),
-          bottom: const TabBar(tabs: [
-            Tab(icon: Icon(Icons.category), text: 'Catagroies'),
-            Tab(icon: Icon(Icons.star), text: 'Favorites'),
-          ]),
-        ),
-        body: const TabBarView(children: [
-          CategoriesScreen(),
-          FavoritesScreen()
-        ],),
-      )
+    return Scaffold(
+      appBar: AppBar(title: Text(_screens[_selectedScreen]['title'])),
+      body: _screens[_selectedScreen]['screen'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectScreen,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        currentIndex: _selectedScreen,
+        type: BottomNavigationBarType.shifting, // Needs to style bar items
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            icon: const Icon(Icons.category), 
+            label: 'Ingredients'
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            icon: const Icon(Icons.star), 
+            label: 'Favorites'
+          ),
+        ],
+      ),
     );
+    // return DefaultTabController(initialIndex: 0, length: 2, child: 
+    //   Scaffold(
+    //     appBar: AppBar(
+    //       title: const Text('DeliMeals'),
+    //       bottom: const TabBar(tabs: [
+    //         Tab(icon: Icon(Icons.category), text: 'Catagroies'),
+    //         Tab(icon: Icon(Icons.star), text: 'Favorites'),
+    //       ]),
+    //     ),
+    //     body: const TabBarView(children: [
+    //       CategoriesScreen(),
+    //       FavoritesScreen()
+    //     ],),
+    //   )
+    // );
   }
 }
