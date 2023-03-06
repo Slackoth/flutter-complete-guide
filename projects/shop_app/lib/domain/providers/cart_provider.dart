1370f0fe672bf84c8e9145e1f161b56f83180a58
Slackoth <00082318@uca.edu.sv>
2023-03-06 17:19:49 -0600

    [ADDED] Cart models and provider

---
+++ projects/shop_app/lib/domain/providers/cart_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/domain/models/cart_item.dart';
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if(_items.containsKey(productId)) {
+      _items.update(productId, (existingItem) => CartItem(
+        id: existingItem.id,
+        title: existingItem.title,
+        quantity: existingItem.quantity + 1,
+        price: existingItem.price
+      ));
+    }
+    else {
+      _items.putIfAbsent(productId,
+      () => CartItem(
+        id: DateTime.now().toIso8601String(),
+        price: price, 
+        quantity: 1,
+        title: title
+      ));
+    }
+  }
+}
