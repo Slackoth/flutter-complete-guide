7421527e0e87c8f4ccbf897d3322984025b4a600
Slackoth <00082318@uca.edu.sv>
2023-03-06 18:42:50 -0600

    [ADDED] Product details

--- projects/shop_app/lib/screens/product_detail_screen.dart
+++ projects/shop_app/lib/screens/product_detail_screen.dart
@@ -20,6 +20,35 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(product.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(product.imageUrl, fit: BoxFit.cover,),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              '${product.price}',
+              style: const TextStyle(
+                fontSize: 20,
+                color: Colors.grey
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                product.description,
+                softWrap: true,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
